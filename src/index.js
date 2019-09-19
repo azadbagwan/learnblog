a5e3a734828147dee456b8dfdeff8213a55b1638
azadbagwan <azadbagwan2@yahoo.com>
2019-09-20 03:10:22 +0530

    commit 18/9/2019 13:35

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import 'react-app-polyfill/ie11'
+import 'core-js/features/array/find';
+import 'core-js/features/array/includes';
+import 'core-js/features/number/is-nan';
 import ReactDOM from "react-dom";
 //import './index.css';
 import { HashRouter as Router } from "react-router-dom";
